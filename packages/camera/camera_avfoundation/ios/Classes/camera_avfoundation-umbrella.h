c3dcf11f01ee414d3f5fa92fc69c14ab723d8f84
Huan Lin <hellohuanlin@gmail.com>
2022-06-16 17:06:33 -0700

    remove umbrella header at all

--- packages/camera/camera_avfoundation/ios/Classes/camera_avfoundation-umbrella.h
+++ packages/camera/camera_avfoundation/ios/Classes/camera_avfoundation-umbrella.h
@@ -3,14 +3,14 @@
 // found in the LICENSE file.
 
 #import <Foundation/Foundation.h>
-#import <camera_avfoundation/CameraPlugin.h>
+//#import <camera_avfoundation/CameraPlugin.h>
 
 FOUNDATION_EXPORT double cameraVersionNumber;
 FOUNDATION_EXPORT const unsigned char cameraVersionString[];
 
-#import <camera_avfoundation/FLTThreadSafeTextureRegistry.h>
-#import <camera_avfoundation/QueueUtils.h>
-#import <camera_avfoundation/FLTThreadSafeMethodChannel.h>
-#import <camera_avfoundation/FLTCam.h>
+//#import <camera_avfoundation/FLTThreadSafeTextureRegistry.h>
+//#import <camera_avfoundation/QueueUtils.h>
+//#import <camera_avfoundation/FLTThreadSafeMethodChannel.h>
+//#import <camera_avfoundation/FLTCam.h>
 
-#import <camera_avfoundation/CameraProperties.h>
+//#import <camera_avfoundation/CameraProperties.h>
